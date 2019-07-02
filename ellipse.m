18b9a9119797eb69985446592ea7bea911305038
Bruce D. Kothmann <bruce.d.kothmann@gmail.com>
2019-07-02 16:05:41 -0400

    Added Control of Angle Range

--- ellipse.m
+++ ellipse.m
@@ -5,18 +5,20 @@ classdef ellipse < handle
         yc
         a
         b
+        qVec
     end
     
     methods
-        function this = ellipse(x,y,xscale,yscale)
+        function this = ellipse(x,y,xscale,yscale,q)
             this.xc = x;
             this.yc = y;
             this.a = xscale;
             this.b = yscale;
+            this.qVec = q;
         end
         
         function render(this)
-            t = [0:pi/500:2*pi];
+            t = this.qVec;
             x = this.xc + this.a*cos(t);
             y = this.yc + this.b*sin(t);
             plot(x,y,'LineWidth',2);
