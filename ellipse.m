90da14c8790019963025177835961f77630908d4
Bruce D. Kothmann <bruce.d.kothmann@gmail.com>
2019-07-02 15:55:54 -0400

    Color Control Added

--- ellipse.m
+++ ellipse.m
@@ -5,21 +5,23 @@ classdef ellipse < handle
         yc
         a
         b
+        color
     end
     
     methods
-        function this = ellipse(x,y,xscale,yscale)
+        function this = ellipse(x,y,xscale,yscale,c)
             this.xc = x;
             this.yc = y;
             this.a = xscale;
             this.b = yscale;
+            this.color = c;
         end
         
         function render(this)
             t = [0:pi/500:2*pi];
             x = this.xc + this.a*cos(t);
             y = this.yc + this.b*sin(t);
-            plot(x,y,'LineWidth',2);
+            plot(x,y,this.color,'LineWidth',2);
             axis equal; set(gcf,'Color',[1 1 1]);
         end
         
--- ellipseTestScript.m
+++ ellipseTestScript.m
@@ -3,5 +3,5 @@ clear all; close all;
 % BDK: 2019-07-02
 % simple test of the ellipse class
 %
-E = ellipse(2, 5, 1, 4);
+E = ellipse(2, 5, 1, 4, 'g');
 E.render
